837ce49e75884cd00a356da1f1c104f8e05ecacf
EasonChan <mr.chen98@gmail.com>
2017-07-04 10:42:02 -0500

    modify
    modify

---
+++ src/ui/AddNewMemberWindow.java
@@ -0,0 +1,25 @@
+package ui;
+
+import javafx.stage.Stage;
+
+public class AddNewMemberWindow extends Stage implements LibWindow {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean isInitialized() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void isInitialized(boolean val) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
