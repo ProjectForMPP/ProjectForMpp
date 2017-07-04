245ece32949b5fbd1c8c19ce844ddb92ea778784
EasonChan <mr.chen98@gmail.com>
2017-07-04 12:13:16 -0500

    add new class
    add new class

--- src/ui/AddNewMemberWindow.java
+++ src/ui/AddNewMemberWindow.java
@@ -1,25 +1,69 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class AddNewMemberWindow extends Stage implements LibWindow {
-
+	public static final AddNewMemberWindow INSTANCE = new AddNewMemberWindow();
+	private boolean isInitialized = false;
+	public boolean isInitialized() {
+		return isInitialized;
+	}
+	public void isInitialized(boolean val) {
+		isInitialized = val;
+	}
+	private TextArea ta;
+	public void setData(String data) {
+		ta.setText(data);
+	}
+	private AddNewMemberWindow() {}
+	
 	@Override
 	public void init() {
 		// TODO Auto-generated method stub
 
+		GridPane grid = new GridPane();
+		grid.setId("top-container");
+		grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Add NewMember");
+        scenetitle.setFont(Font.font("Harlow Solid Italic", FontWeight.NORMAL, 20)); //Tahoma
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+		ta = new TextArea();
+		grid.add(ta, 0,1);	
+		
+		Button backBtn = new Button("<= Back to Main");
+        backBtn.setOnAction(new EventHandler<ActionEvent>() {
+        	@Override
+        	public void handle(ActionEvent e) {
+        		Start.hideAllWindows();
+        		Start.primStage().show();
+        	}
+        });
+        HBox hBack = new HBox(10);
+        hBack.setAlignment(Pos.BOTTOM_LEFT);
+        hBack.getChildren().add(backBtn);
+        grid.add(hBack, 0, 2);
+		Scene scene = new Scene(grid);
+		scene.getStylesheets().add(getClass().getResource("library.css").toExternalForm());
+        setScene(scene);
 	}
 
-	@Override
-	public boolean isInitialized() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void isInitialized(boolean val) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
