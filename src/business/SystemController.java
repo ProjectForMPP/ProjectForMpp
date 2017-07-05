ba90a156088c5721d00db453bc2cdd20368a5323
EasonChan <mr.chen98@gmail.com>
2017-07-05 15:58:18 -0500

    新增功能
    修改冲突代码

--- src/business/SystemController.java
+++ src/business/SystemController.java
@@ -15,10 +15,6 @@ import dataaccess.User;
 
 public class SystemController implements ControllerInterface {
 
-<<<<<<< HEAD
-=======
-
->>>>>>> a0c65e1a7b22cf2619dc5b720cfd17e84994470e
 	public static Auth currentAuth = null;
 	
 	public void checkoutBook(String memberId,String ISBN) throws LibrarySystemException{
@@ -45,12 +41,9 @@ public class SystemController implements ControllerInterface {
 		DataAccessFacade.saveToStorage(DataAccessFacade.StorageType.MEMBERS, libraryMember);
 		DataAccessFacade.saveToStorage(DataAccessFacade.StorageType.BOOKS, book);
 	}
+	
+	
 	@Override
-
-<<<<<<< HEAD
-=======
-//>>>>>>> 63455028fbb9a7011666c7cf822c4c13baac16b5
->>>>>>> a0c65e1a7b22cf2619dc5b720cfd17e84994470e
 	public Auth login(String id, String password) throws LoginException {
 		DataAccess da = new DataAccessFacade();
 		HashMap<String, User> map = da.readUserMap();
@@ -61,17 +54,8 @@ public class SystemController implements ControllerInterface {
 		if (!passwordFound.equals(password)) {
 			throw new LoginException("Password incorrect");
 		}
-<<<<<<< HEAD
-		return map.get(id).getAuthorization();
-=======
-//<<<<<<< HEAD
-		return map.get(id).getAuthorization();
 
-//=======
-//		currentAuth = map.get(id).getAuthorization();
-//		return currentAuth;
-//>>>>>>> 63455028fbb9a7011666c7cf822c4c13baac16b5
->>>>>>> a0c65e1a7b22cf2619dc5b720cfd17e84994470e
+		return map.get(id).getAuthorization();
 	}
 
 	@Override
@@ -89,15 +73,7 @@ public class SystemController implements ControllerInterface {
 		retval.addAll(da.readBooksMap().keySet());
 		return retval;
 	}
-<<<<<<< HEAD
 
-=======
-//<<<<<<< HEAD
-
-//=======
-	
-//>>>>>>> 63455028fbb9a7011666c7cf822c4c13baac16b5
->>>>>>> a0c65e1a7b22cf2619dc5b720cfd17e84994470e
 	@Override
 	public void addNewLibaryMember(LibraryMember member) throws LibrarySystemException {
 
@@ -116,11 +92,6 @@ public class SystemController implements ControllerInterface {
 
 		members.add(member);
 		DataAccessFacade.loadMemberMap(members);
-<<<<<<< HEAD
-=======
-//<<<<<<< HEAD
->>>>>>> a0c65e1a7b22cf2619dc5b720cfd17e84994470e
-
 	}
 
 	@Override
@@ -159,79 +130,5 @@ public class SystemController implements ControllerInterface {
 		authors.add(author);
 		DataAccessFacade.loadAuthorMap(authors);
 	}
-
-<<<<<<< HEAD
-	}
-
-=======
-//=======
-	}
-//>>>>>>> 63455028fbb9a7011666c7cf822c4c13baac16b5
-//}
-
-//=======
-//package business;
-//
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//
-//import business.Book;
-//import dataaccess.Auth;
-//import dataaccess.DataAccess;
-//import dataaccess.DataAccessFacade;
-//import dataaccess.User;
-//
-//public class SystemController implements ControllerInterface {
-//	
-//	public Auth login(String id, String password) throws LoginException {
-//		DataAccess da = new DataAccessFacade();
-//		HashMap<String, User> map = da.readUserMap();
-//		if(!map.containsKey(id)) {
-//			throw new LoginException("ID " + id + " not found");
-//		}
-//		String passwordFound = map.get(id).getPassword();
-//		if(!passwordFound.equals(password)) {
-//			throw new LoginException("Password incorrect");
-//		}
-//		return map.get(id).getAuthorization();
-//		
-//	}
-//	@Override
-//	public List<String> allMemberIds() {
-//		DataAccess da = new DataAccessFacade();
-//		List<String> retval = new ArrayList<>();
-//		retval.addAll(da.readMemberMap().keySet());
-//		return retval;
-//	}
-//	
-//	@Override
-//	public List<String> allBookIds() {
-//		DataAccess da = new DataAccessFacade();
-//		List<String> retval = new ArrayList<>();
-//		retval.addAll(da.readBooksMap().keySet());
-//		return retval;
-//	}
-//	@Override
-//	public void addNewLibaryMember(LibraryMember member) throws LibrarySystemException {
-//		// TODO Auto-generated method stub
-//		System.out.println("--------"+member.getFirstName());
-//		
-//		DataAccess da = new DataAccessFacade();
-//		HashMap<String, User> map = da.readUserMap();
-//		System.out.println("----:"+map);
-//		if(map.containsKey(member.getMemberId())) {
-//			throw new LibrarySystemException("ID " + member.getMemberId() + " is existence");
-//		}
-//		
-//		List<LibraryMember> members = new ArrayList<LibraryMember>();
-//		members.add(member);
-//		DataAccessFacade.loadMemberMap(members);
-//		
-//	}
-//	
-//	
-//}
-//>>>>>>> 04a0959c7a61f48be86d4b7773a4f5e473a0147d
->>>>>>> a0c65e1a7b22cf2619dc5b720cfd17e84994470e
+}
 
